05087ed99b1f517637c7303c83d5c9a08e7035f1
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-09-09 11:01:48 +0100

    custom connect button updated

--- components/Global/CustomConnectButton.jsx
+++ components/Global/CustomConnectButton.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { RiWallet3Line } from "react-icons/ri";
@@ -20,27 +21,52 @@ const CustomConnectButton = ({ active, childStyle }) => {
           <div
             {...(!ready && {
               "aria-hidden": true,
-              style: {
-                opacity: 0,
-                pointerEvents: "none",
-                userSelect: "none",
-              },
+              style: { opacity: 0, pointerEvents: "none", userSelect: "none" },
             })}
           >
-            {() => {
-              if (!connected) {
-                return (
+            {!connected ? (
+              <button
+                onClick={openConnectModal}
+                className={`flex items-center bg-gradient-to-r from-fuchsia-500 to-purple-600 hover:from-fuchsia-600 hover:to-purple-700 text-white px-4 py-2 rounded-md transition-colors ${childStyle}`}
+              >
+                <RiWallet3Line className="mr-2" size={20} />
+                CONNECT WALLET
+              </button>
+            ) : chain?.unsupported ? (
+              <button
+                onClick={openChainModal}
+                className="bg-red-500 hover:bg-red-600 text-white px-6 py-2 rounded-lg transition-colors"
+              >
+                Wrong network
+              </button>
+            ) : (
+              <div className="flex items-center gap-4">
+                {active && (
                   <button
-                    onClick={openConnectModal}
-                    className={`
-                        flex items-center bg-gradient-to-r from-fuchsia-500 to-purple-600 hover:from-fuchsia-600 hover:to-purple-700 text-white px-4 py-2 rounded-md transition-colors ${childStyle}`}
+                    onClick={openChainModal}
+                    className="bg-gradient-to-r from-fuchsia-500 to-purple-600 hover:from-fuchsia-600 hover:to-purple-700 text-white px-4 py-2 rounded-lg flex items-center gap-2"
                   >
-                    <RiWallet3Line className="mr-2" size={20} />
-                    CONNECT WALLET
+                    {chain?.hasIcon && chain?.iconUrl && (
+                      <img
+                        src={chain.iconUrl}
+                        alt={chain.name ?? "Chain icon"}
+                        className="size-5"
+                      />
+                    )}
                   </button>
-                );
-              }
-            }}
+                )}
+
+                <button
+                  onClick={openAccountModal}
+                  className="bg-gradient-to-r from-fuchsia-500 to-purple-600 hover:from-fuchsia-600 hover:to-purple-700 text-white px-4 py-2 rounded-lg flex items-center gap-2"
+                >
+                  {account?.displayName}
+                  {account?.displayBalance
+                    ? ` (${account.displayBalance})`
+                    : null}
+                </button>
+              </div>
+            )}
           </div>
         );
       }}
